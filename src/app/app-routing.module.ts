dc444282c399ff0934a9a235f0b9055db8dcc226
HimanshuKumar <71648263+himanshumahuri@users.noreply.github.com>
2022-10-04 13:59:22 +0530

    added survey form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { SurveyComponent } from './components/survey/survey.component';
 
 const routes: Routes = [
   {path:'',component: HomeComponent},
   {path:'signup',component: RegisterComponent},
   {path:'login',component:LoginComponent },
+  {path:'survey',component:SurveyComponent },
 
 ];
 @NgModule({
